e333c920cbf48d20ca33aa0bec7cc081993c1333
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-05-22 17:09:07 +0700

    feat: add custom domain in cloudflare

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,5 @@
 import type { SSTConfig } from "sst";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { App, AstroSite } from "sst/constructs";
 
 export default {
@@ -10,8 +11,21 @@ export default {
   },
   stacks(app: App) {
     app.stack(function Site({ stack }) {
-      const site = new AstroSite(stack, "web");
+      const site = new AstroSite(stack, "web", {
+        customDomain: {
+          domainName: "crane.ivokun.com",
+          isExternalDomain: true,
+          cdk: {
+            certificate: Certificate.fromCertificateArn(
+              stack,
+              "CraneCert",
+              process.env.CERT_ARN ?? ""
+            ),
+          },
+        },
+      });
       stack.addOutputs({
+        prodUrl: site.customDomainUrl,
         url: site.url,
       });
     });
